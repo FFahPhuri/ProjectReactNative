2fa21d886bbfecc9f516e60809644dbd017dcbea
FFahPhuri <85545858+FFahPhuri@users.noreply.github.com>
2021-12-08 11:56:57 +0700

    Add files via upload

---
+++ 08122021/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+//import TextInputReview from'./components/TextInputReview';
+//import ImageWithTextInput from "./components/ImageWithTextInput";
+//import ButtonExample from "./components/ButtonExample";
+//import TextInputPractice from "./components/TextInputPractice1";
+//import TouchableExample from "./components/TouchableExample"
+import TouchablePractice from "./components/TouchablePractice"
+
+const App = () => {
+  return (
+    <TouchablePractice/>
+  );
+};
+
+export default App;
