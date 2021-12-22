a3e44a96a30d696f6054751bdf56ca923ab0c3d6
FFahPhuri <85545858+FFahPhuri@users.noreply.github.com>
2021-12-22 11:45:58 +0700

    Add files via upload
    WebView and Stack Navigation

--- App.js
+++ App.js
@@ -1,25 +1,44 @@
 import React from 'react'
-import { View, Text , Button } from 'react-native'
-// import Logo from './components/Logo'
-// import Footer from './components/Footer'
-// import AlertExample from './components/AlertExample'
-import ModalExample from './components/ModalExample'
+import { View, Text, Button } from 'react-native'
+//import WebView from 'react-native-webview'
+//import WebViewExample from './components/WebViewExample'
+//import WebViewLoading from './components/WebViewLoading'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import AboutScreen from './screens/AboutScreen';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
-  // const showData = () =>{
-  //   alert('Hello world!!!')
-  // }
+
   return (
-    /* <Logo/>
-      <Text>Hello React Native</Text>
-      <Text>สวัสดี React Native</Text>
-      <Button title='Click' onPress={showData}/>
-      <Footer/> */
-      
-    // <AlertExample/>
-    <ModalExample/>
-    
-    
+    //<WebViewExample/>
+    //<WebViewLoading/>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName='Home'
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#654321',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'หน้าหลัก' }} />
+        <Stack.Screen
+          name="About"
+          component={AboutScreen}
+          options={{ title: 'เกี่ยวกับเรา' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
   )
 }
 
