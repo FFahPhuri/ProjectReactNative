5842d70d0e737d1aaa006b09671170ddc097e2af
FFahPhuri <85545858+FFahPhuri@users.noreply.github.com>
2022-01-07 19:56:57 +0700

    Add files via upload

--- screens/FirstPage.js
+++ screens/FirstPage.js
@@ -1,52 +1,51 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
-const FirstPage = ({ navigation }) => {
-    const [InputName, setInputName] = useState('');
-    return ( 
-        <View style = { styles.container } >
-        
-        <Text style = { styles.heading } > REACT - NATIVE PASS VALUE FROM ONE SCREEN </Text> 
-        <Text style = { styles.textStyle } > Please insert your name to pass it to second screen </Text> 
-        <TextInput 
-        style = { styles.inputStyle }
-        placeholder = "Enter Your Name"
-        onChangeText = {
-            (InputName) => { setInputName(Name) } }
-        /> 
-        <Button title = "GO NEXT"
-        onPress = {() => navigation.navigate('SecondPage', { name: InputName }) }
-        /> 
-        </View>
-    );
+const FirstPage = ({navigation}) => {
+    const [Name, setName] = useState('');
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>REACT-NATIVE PASS VALUE FROM ONE SCREEN</Text>
+      <Text style={styles.textStyle}>Please insert your name to pass it to second screen</Text>
+      <TextInput
+        style={styles.inputStyle}
+        placeholder="Enter Your Name"
+        onChangeText={(Name) => {setName(Name)}}
+      />
+       <Button
+        title="GO NEXT"
+        onPress={() => navigation.navigate('SecondPage',{name: Name})}
+      />
+    </View>
+  );
 };
 
 export default FirstPage;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        padding: 20,
-    },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 20,
+  },
 
-    heading: {
-        fontSize: 25,
-        textAlign: 'center',
-        marginVertical: 10,
-    },
+  heading: {
+    fontSize: 25,
+    textAlign: 'center',
+    marginVertical: 10,
+  },
 
-    textStyle: {
-        textAlign: 'center',
-        fontSize: 16,
-        marginVertical: 10,
-    },
+  textStyle: {
+    textAlign: 'center',
+    fontSize: 16,
+    marginVertical: 10,
+  },
 
-    inputStyle: {
-        width: '80%',
-        height: 44,
-        padding: 10,
-        marginVertical: 10,
-        backgroundColor: '#DBDBD6',
-    },
+  inputStyle: {
+    width: '80%',
+    height: 44,
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#DBDBD6',
+  },
 });
